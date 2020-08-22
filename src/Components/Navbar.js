f7b98913ac2f0cc10cc9ff9cbf26ba90c157412a
masham04 <66207787+masham04@users.noreply.github.com>
2020-08-22 17:01:46 +0500

    final update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,7 +4,8 @@ import '../App.css'
 export const Navbar = () => {
   return (
     <div className={'title'}>
-      <h1 className={'title-text'}>C<span id={'space'}><img id={'img'} alt='"virus logo" src={logo} width={'100px'}></img></span>VID-19</h1>
+      <h1 className={'title-text'}>C<span id={'space'}>
+        <img id={'img'} alt='virus logo' src={logo} width={'100px'}></img></span>VID-19</h1>
       
     </div>
   )
